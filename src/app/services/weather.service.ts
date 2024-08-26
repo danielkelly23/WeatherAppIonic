a1eaab1066f6ffa538d5c263a80d5e47929b9c10
DANIEL KELLY <G00421651@atu.ie>
2024-08-26 22:11:15 +0100

    updated fixed Api call

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';//Import HttpClient to service
+import { HttpClient } from '@angular/common/http';//Import HttpClient to service
 import { Observable } from 'rxjs'; //
 
 @Injectable({
@@ -11,8 +11,8 @@ export class WeatherService {
 
   getWeatherData(): Observable<any>
   {
-    let lat = 42.9832406;
-    let lon = -81.243372;
+    let lat = 53.350140;//This is the Latitude for Dublin
+    let lon = -6.266155;//This is the Longitude for Dublin
     let apiId = '5276290338834bcaf97995c74dab83ca';
     let queryString = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiId}`;
     return this.http.get(queryString); //Return http get call to call the query string

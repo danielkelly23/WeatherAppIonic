ae57e9f93d5f6c7ece6020b8a11481a950cd3f42
DANIEL KELLY <G00421651@atu.ie>
2024-08-26 21:20:34 +0100

    setting up Api key for weather data

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WeatherService } from '../services/weather.service';//import weather service from weather.service
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  private res: any;
+
+  constructor(private weatherAPI:WeatherService) {}
+
+  ngOnInit(){
+      this.getWeatherData();
+  }
+
+  getWeatherData(){
+    this.weatherAPI.getWeatherData().subscribe((response) => {
+      this.res = response;
+      console.log(this.res);
+    });
+  }
+
 
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';//Import HttpClient to service
+import { Observable } from 'rxjs'; //
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private http: HttpClient,) { }
+
+  getWeatherData(): Observable<any>
+  {
+    let lat = 42.9832406;
+    let lon = -81.243372;
+    let apiId = '5276290338834bcaf97995c74dab83ca';
+    let queryString = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiId}`;
+    return this.http.get(queryString); //Return http get call to call the query string
+  }
 }
